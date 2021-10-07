68493a4bcd1858d35abbaae1e09b1ba2fa1a8590
pooyan <goodarzipooyan@gmail.com>
2021-10-07 12:10:50 +0330

    test

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ hugo
 sudo cp -r public/* ../pooyangoodarzi.github.io/
 
 git add -A
-git commit -a -m '$1'
+git commit -a -m "$1"
 git push 
 
 cd ../pooyangoodarzi.github.io/
 git add -A
-git commit -a -m '$1'
+git commit -a -m "$1"
 git push
 
 
